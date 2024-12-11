e269aeb449deb8dc8104fee055e0fa4defc37c99
Harapko <harapkomaskym@gmail.com>
2024-12-11 02:19:03 +0200

    unit-test-for-additional-content

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/AdditionalContent/Coordinate/UpdateCoordinateHandlerTest.cs
@@ -0,0 +1,75 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+using Streetcode.BLL.DTO.AdditionalContent.Coordinates.Types;
+using Streetcode.BLL.MediatR.AdditionalContent.Coordinate.Create;
+using Streetcode.BLL.MediatR.AdditionalContent.Coordinate.Update;
+using Xunit;
+using StreetcodeCoordinateEntity = Streetcode.DAL.Entities.AdditionalContent.Coordinates.Types.StreetcodeCoordinate;
+
+
+namespace Streetcode.XUnitTest.MediatRTests.AdditionalContent.Coordinate;
+
+public class UpdateCoordinateHandlerTest : AdditionalContentTestWrapper
+{
+    private readonly UpdateCoordinateHandler _handler;
+
+    public UpdateCoordinateHandlerTest()
+    {
+        _handler = new UpdateCoordinateHandler(_repositoryWrapperMock.Object, _mapperMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnOkResult_WhenCoordinateAreUpdate()
+    {
+        // Arrange
+        var streetcodeCoordinateDto = new StreetcodeCoordinateDTO { StreetcodeId = 1 };
+        var streetcodeCoordinate = new StreetcodeCoordinateEntity { StreetcodeId = 1 };
+        var request = new UpdateCoordinateCommand(streetcodeCoordinateDto);
+
+        _mapperMock.Setup(m => m.Map<StreetcodeCoordinateEntity>(streetcodeCoordinateDto))
+            .Returns(streetcodeCoordinate);
+
+        _repositoryWrapperMock.Setup(rep => rep.StreetcodeCoordinateRepository.Update(streetcodeCoordinate));
+        _repositoryWrapperMock.Setup(rep => rep.SaveChangesAsync())
+            .ReturnsAsync(1);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+
+        _repositoryWrapperMock.Verify(rep => rep.StreetcodeCoordinateRepository.Update(streetcodeCoordinate), Times.Once);
+        _mapperMock.Verify(m => m.Map<StreetcodeCoordinateEntity>(streetcodeCoordinateDto), Times.Once);
+        _repositoryWrapperMock.Verify(rep => rep.SaveChangesAsync(), Times.Once());
+    }
+    
+    [Fact]
+    public async Task Handle_ReturnOkResult_WhenCoordinateAreNotUpdate()
+    {
+        // Arrange
+        var streetcodeCoordinateDto = new StreetcodeCoordinateDTO { StreetcodeId = 1 };
+        var streetcodeCoordinate = new StreetcodeCoordinateEntity { StreetcodeId = 1 };
+        var request = new UpdateCoordinateCommand(streetcodeCoordinateDto);
+        const string errMsg = "Failed to update a streetcodeCoordinate";
+
+        _mapperMock.Setup(m => m.Map<StreetcodeCoordinateEntity>(streetcodeCoordinateDto))
+            .Returns(streetcodeCoordinate);
+
+        _repositoryWrapperMock.Setup(rep => rep.StreetcodeCoordinateRepository.Update(streetcodeCoordinate));
+        _repositoryWrapperMock.Setup(rep => rep.SaveChangesAsync())
+            .ReturnsAsync(0);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsFailed);
+        Assert.Equal(errMsg, result.Errors[0].Message);
+
+        _repositoryWrapperMock.Verify(rep => rep.StreetcodeCoordinateRepository.Update(streetcodeCoordinate), Times.Once);
+        _mapperMock.Verify(m => m.Map<StreetcodeCoordinateEntity>(streetcodeCoordinateDto), Times.Once);
+        _repositoryWrapperMock.Verify(rep => rep.SaveChangesAsync(), Times.Once());
+    }
+
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/AdditionalContent/Tag/GetTagsByIdHandlerTest.cs
@@ -0,0 +1,74 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.BLL.DTO.AdditionalContent;
+using Streetcode.BLL.MediatR.AdditionalContent.Tag.GetById;
+using Xunit;
+using TagEntity = Streetcode.DAL.Entities.AdditionalContent.Tag;
+
+
+namespace Streetcode.XUnitTest.MediatRTests.AdditionalContent.Tag;
+
+public class GetTagsByIdHandlerTest : AdditionalContentTestWrapper
+{
+    private readonly GetTagByIdHandler _handler;
+
+    public GetTagsByIdHandlerTest()
+    {
+        _handler = new GetTagByIdHandler(_repositoryWrapperMock.Object, _mapperMock.Object, _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnOkResult_WhenTagsByIdAreFound()
+    {
+        // Arrange
+        var tagId = 1;
+        var request = new GetTagByIdQuery(tagId);
+        var tag = new TagEntity { Id = 1, Title = "Test tag 1" };
+        var tagDto = new TagDTO { Id = 1, Title = "Tag test 1" };
+
+        _repositoryWrapperMock.Setup(rep => rep.TagRepository.GetFirstOrDefaultAsync(
+                It.Is<Expression<Func<TagEntity, bool>>>(exp => exp.Compile().Invoke(tag)),
+                It.IsAny<Func<IQueryable<TagEntity>, IIncludableQueryable<TagEntity, object>>>()))
+            .ReturnsAsync(tag);
+
+        _mapperMock.Setup(m => m.Map<TagDTO>(tag))
+            .Returns(tagDto);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(tagDto, result.Value);
+
+        _repositoryWrapperMock.Verify(
+            rep => rep.TagRepository.GetFirstOrDefaultAsync(
+            It.Is<Expression<Func<TagEntity, bool>>>(exp => exp.Compile().Invoke(new TagEntity { Id = request.Id })),
+            It.IsAny<Func<IQueryable<TagEntity>, IIncludableQueryable<TagEntity, object>>>()), Times.Once);
+
+        _mapperMock.Verify(m => m.Map<TagDTO>(tag), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnOkResult_WhenTagsByIdNotFound()
+    {
+        // Arrange
+        var tagId = 1;
+        var request = new GetTagByIdQuery(tagId);
+        string errorMsg = $"Cannot find a Tag with corresponding id: {request.Id}";
+
+        _repositoryWrapperMock.Setup(rep => rep.TagRepository.GetFirstOrDefaultAsync(
+                It.IsAny<Expression<Func<TagEntity, bool>>>(),
+                It.IsAny<Func<IQueryable<TagEntity>, IIncludableQueryable<TagEntity, object>>>()))
+            .ReturnsAsync((TagEntity)null!);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(errorMsg, result.Errors[0].Message);
+
+    }
+
+}

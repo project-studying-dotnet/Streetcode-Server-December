b72b3cada12a3d64bd5742957263c90bd0f6f069
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2024-12-13 01:01:23 +0200

    feat/SSAD-74/added-deletefact-controller

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Delete;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetAll;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetById;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetByStreetcodeId;
@@ -8,21 +9,27 @@ namespace Streetcode.WebApi.Controllers.Streetcode.TextContent;
 
 public class FactController : BaseApiController
 {
-    [HttpGet]
-    public async Task<IActionResult> GetAll()
-    {
-        return HandleResult(await Mediator.Send(new GetAllFactsQuery()));
-    }
+	[HttpGet]
+	public async Task<IActionResult> GetAll()
+	{
+		return HandleResult(await Mediator.Send(new GetAllFactsQuery()));
+	}
 
-    [HttpGet("{id:int}")]
-    public async Task<IActionResult> GetById([FromRoute] int id)
-    {
-        return HandleResult(await Mediator.Send(new GetFactByIdQuery(id)));
-    }
+	[HttpGet("{id:int}")]
+	public async Task<IActionResult> GetById([FromRoute] int id)
+	{
+		return HandleResult(await Mediator.Send(new GetFactByIdQuery(id)));
+	}
 
-    [HttpGet("{streetcodeId:int}")]
-    public async Task<IActionResult> GetByStreetcodeId([FromRoute] int streetcodeId)
-    {
-        return HandleResult(await Mediator.Send(new GetFactByStreetcodeIdQuery(streetcodeId)));
-    }
+	[HttpGet("{streetcodeId:int}")]
+	public async Task<IActionResult> GetByStreetcodeId([FromRoute] int streetcodeId)
+	{
+		return HandleResult(await Mediator.Send(new GetFactByStreetcodeIdQuery(streetcodeId)));
+	}
+
+	[HttpDelete("{id:int}")]
+	public async Task<IActionResult> DeleteFact([FromRoute] int id)
+	{
+		return HandleResult(await Mediator.Send(new DeleteFactQuery(id)));
+	}
 }

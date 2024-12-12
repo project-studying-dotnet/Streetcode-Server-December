62fbb08d242433e6af5d4bf426406c70669ef958
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2024-12-13 00:37:25 +0200

    feat/SSAD-74/added-deletefact-command

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Delete/DeleteFactCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Delete
+{
+	public record DeleteFactCommand(int id) : IRequest<Result<FactDto>> { }
+}

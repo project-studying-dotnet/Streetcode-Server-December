4b42aa1c5627db331ff7e02eb7b9374c5beb90fc
synapsetron <aleksandr.maslov.job@gmail.com>
2024-12-12 19:05:06 +0200

    feat/SSAD-73:  added UpdateFactCommand and UpdateFactHandler. feat/SSAD-73:  added Update method to FactController.cs test/SSAD-73:  added unit tests for UpdateFactHandler

---
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/Fact/FactController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Update;
+
+namespace Streetcode.WebApi.Controllers.Streetcode.Fact
+{
+    public class FactController : BaseApiController
+    {
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] FactDto fact)
+        {
+            return HandleResult(await Mediator.Send(new UpdateFactCommand(fact)));
+        }
+    }
+}

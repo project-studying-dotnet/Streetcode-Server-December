33b00291d50d12d60799ac5d9ace9c70364b1cd6
Olia <helga.t@i.ua>
2024-12-13 21:53:34 +0200

    feat/71: create endpoint, add CreateFactCommand and CreateFactHandler

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Create/CreateFactCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Create
+{
+    public record CreateFactCommand(FactDto Fact) : IRequest<Result<FactDto>>
+    {
+    }
+}

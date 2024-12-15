10871d791077413e3ae3d13cd79b56cb4c5ad8db
KolyaParnak <hgy.hgyich@gmail.com>
2024-12-15 23:49:40 +0200

    Implemented model validation pipeline

---
+++ Streetcode/Streetcode.BLL/Validation/Validators/CommandsAndQuerysValidators/CreatePartnerQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Partners.Create;
+using Streetcode.BLL.Validation.Validators.DTOValidators.Partners;
+
+namespace Streetcode.BLL.Validation.Validators.CommandsAndQuerysValidators
+{
+    public class CreatePartnerQueryValidator : AbstractValidator<CreatePartnerQuery>
+    {
+        public CreatePartnerQueryValidator()
+        {
+            RuleFor(x => x.newPartner)
+                .NotNull().WithMessage("Partner information cannot be null")
+                .SetValidator(new CreatePartnerDTOValidator());
+        }
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Validation/Validators/DTOValidators/Art/Audio/AudioFileBaseCreateDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Streetcode.BLL.DTO.Media.Audio;
+
+namespace Streetcode.BLL.Validation.Validators.DTOValidators.Art.Audio
+{
+    public class AudioFileBaseCreateDTOValidator : AbstractValidator<AudioFileBaseCreateDTO>
+    {
+        public AudioFileBaseCreateDTOValidator()
+        {
+            Include(new FileBaseCreateDTOValidator("audio"));
+
+            RuleFor(x => x.Description)
+                .MaximumLength(400).WithMessage("The 'Description' field cannot be longer than 400 characters");
+        }
+    }
+}

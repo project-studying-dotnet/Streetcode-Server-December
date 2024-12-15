21f9e181f8b44975bad1dc2229052abc57966eca
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2024-12-15 17:10:31 +0200

    feat/90: implement Create part for Admin/Art gallery block

---
+++ Streetcode/Streetcode.BLL/MediatR/Media/Art/Create/CreateArtCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Streetcode.BLL.DTO.Media.Art;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentResults;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.MediatR.Media.Art.Create
+{
+    public record CreateArtCommand(ArtCreateDTO newArt) : IRequest<Result<ArtDTO>>;
+}

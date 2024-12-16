fb12d6a7a76d0e138deb7fe740a18962c6d7d088
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2024-12-17 01:16:32 +0200

    feat/SSAD-86/createcategorycontent-handler-updated

--- Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/CreateCategoryContent/CreateCategoryContentHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/CreateCategoryContent/CreateCategoryContentHandler.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Streetcode.BLL.MediatR.Sources.SourceLinkCategory.CreateCategoryContent
 {
@@ -30,25 +31,24 @@ namespace Streetcode.BLL.MediatR.Sources.SourceLinkCategory.CreateCategoryConten
 		{
 			var newContent = _mapper.Map<StreetcodeCategoryContent>(request.newContent);
 
-			try
+			newContent = await _repositoryWrapper.StreetcodeCategoryContentRepository.CreateAsync(newContent);
+
+			newContent.SourceLinkCategory = await _repositoryWrapper.SourceCategoryRepository
+				.GetSingleOrDefaultAsync(s => s.Id == newContent.SourceLinkCategoryId);
+			newContent.Streetcode = await _repositoryWrapper.StreetcodeRepository
+				.GetSingleOrDefaultAsync(s => s.Id == newContent.StreetcodeId);
+
+			var resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
+
+			if (!resultIsSuccess)
 			{
-				newContent = await _repositoryWrapper.StreetcodeCategoryContentRepository.CreateAsync(newContent);
-
-				newContent.SourceLinkCategory = await _repositoryWrapper.SourceCategoryRepository
-					.GetSingleOrDefaultAsync(s => s.Id == newContent.SourceLinkCategoryId);
-				newContent.Streetcode = await _repositoryWrapper.StreetcodeRepository
-					.GetSingleOrDefaultAsync(s => s.Id == newContent.StreetcodeId);
-
-				_repositoryWrapper.SaveChanges();
-
-				var resultDto = _mapper.Map<CategoryContentCreateDTO>(newContent);
-				return Result.Ok(resultDto);
+				const string errorMsg = "Failed to create source records";
+				_logger.LogError(request, errorMsg);
+				return Result.Fail(new Error(errorMsg));
 			}
-			catch (Exception ex)
-			{
-				_logger.LogError($"Error occurred while creating StreetcodeCategoryContent: {ex.Message}", ex.Message);
-				return Result.Fail("An error occurred while creating the content.");
-			}
+
+			var resultDto = _mapper.Map<CategoryContentCreateDTO>(newContent);
+			return Result.Ok(resultDto);
 		}
 	}
 }

23de2d8f6789fa0dd35760e134c799f6ec83fe7b
gorgulia <82047115+gorgulia@users.noreply.github.com>
2024-12-17 23:02:33 +0200

    Implemented Fluent validation for: TimelineItemCreateDTO CreateTimelineItemCommand
    FactCreateDTO
    CreateFactCommand
    
    Covered validators with tests

---
+++ Streetcode/Streetcode.BLL/Validation/Validators/CommandsAndQuerysValidators/CreateTimelineItemCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Timeline.TimelineItem.Create;
+using Streetcode.BLL.Validation.Validators.DTOValidators.Timeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.Validation.Validators.CommandsAndQuerysValidators
+{
+    public class CreateTimelineItemCommandValidator : AbstractValidator<CreateTimelineItemCommand>
+    {
+        public CreateTimelineItemCommandValidator()
+        {
+            RuleFor(x => x.TimelineItem)
+                .NotNull().WithMessage("TimelineItem cannot be null.")
+                .SetValidator(new TimelineItemCreateDTOValidator());
+        }
+    }
+}

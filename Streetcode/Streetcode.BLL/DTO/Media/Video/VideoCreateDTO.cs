5a8ca2a13f2b85b9b991054bf0a51283afd7a17c
KolyaParnak <hgy.hgyich@gmail.com>
2024-12-19 20:26:04 +0200

    feat/149/1/added-createVideoDTO

---
+++ Streetcode/Streetcode.BLL/DTO/Media/Video/VideoCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Streetcode.BLL.DTO.Media.Video
+{
+    public class VideoCreateDTO
+    {
+        [Required]
+        public string? Url { get; set; }
+
+        [Required]
+        public int StreetcodeId { get; set; }
+    }
+}

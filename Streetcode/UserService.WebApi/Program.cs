662a435d973d9d8f3f59d08549c855d9fe5090a4
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2024-12-19 18:33:51 +0200

    Removed WeatherForecast related objects

--- Streetcode/UserService.WebApi/Program.cs
+++ Streetcode/UserService.WebApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 // MongoDB Configuration
-string mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb");
+string mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb")!;
 builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
 {
     return new MongoClient(mongoConnectionString);
@@ -28,28 +28,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

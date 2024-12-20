49f25dcc732cf5601b5891b3d10c162efc33706b
KolyaParnak <hgy.hgyich@gmail.com>
2024-12-20 13:21:45 +0200

    ref/106/part2-BLL-update-namespaces

--- Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/GetById/GetTimelineItemByIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/GetById/GetTimelineItemByIdHandler.cs
@@ -6,37 +6,38 @@ using Streetcode.BLL.DTO.Timeline;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
-namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.GetById;
-
-public class GetTimelineItemByIdHandler : IRequestHandler<GetTimelineItemByIdQuery, Result<TimelineItemDTO>>
+namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.GetById
 {
-    private readonly IMapper _mapper;
-    private readonly IRepositoryWrapper _repositoryWrapper;
-    private readonly ILoggerService _logger;
-
-    public GetTimelineItemByIdHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
+    public class GetTimelineItemByIdHandler : IRequestHandler<GetTimelineItemByIdQuery, Result<TimelineItemDTO>>
     {
-        _repositoryWrapper = repositoryWrapper;
-        _mapper = mapper;
-        _logger = logger;
-    }
+        private readonly IMapper _mapper;
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
 
-    public async Task<Result<TimelineItemDTO>> Handle(GetTimelineItemByIdQuery request, CancellationToken cancellationToken)
-    {
-        var timelineItem = await _repositoryWrapper.TimelineRepository
-            .GetFirstOrDefaultAsync(
-                predicate: ti => true,
-                include: ti => ti
-                    .Include(til => til.HistoricalContextTimelines)
-                        .ThenInclude(x => x.HistoricalContext)!);
-
-        if (timelineItem is null)
+        public GetTimelineItemByIdHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
         {
-            string errorMsg = $"Cannot find a timeline item with corresponding id: {request.Id}";
-            _logger.LogError(request, errorMsg);
-            return Result.Fail(new Error(errorMsg));
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+            _logger = logger;
         }
 
-        return Result.Ok(_mapper.Map<TimelineItemDTO>(timelineItem));
+        public async Task<Result<TimelineItemDTO>> Handle(GetTimelineItemByIdQuery request, CancellationToken cancellationToken)
+        {
+            var timelineItem = await _repositoryWrapper.TimelineRepository
+                .GetFirstOrDefaultAsync(
+                    predicate: ti => true,
+                    include: ti => ti
+                        .Include(til => til.HistoricalContextTimelines)
+                            .ThenInclude(x => x.HistoricalContext)!);
+
+            if (timelineItem is null)
+            {
+                string errorMsg = $"Cannot find a timeline item with corresponding id: {request.Id}";
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+
+            return Result.Ok(_mapper.Map<TimelineItemDTO>(timelineItem));
+        }
     }
 }
--- Streetcode/Streetcode.BLL/MediatR/Toponyms/GetAll/GetAllToponymsQuery.cs
+++ Streetcode/Streetcode.BLL/MediatR/Toponyms/GetAll/GetAllToponymsQuery.cs
@@ -2,7 +2,7 @@
 using MediatR;
 using Streetcode.BLL.DTO.Toponyms;
 
-namespace Streetcode.BLL.MediatR.Toponyms.GetAll;
-
-public record GetAllToponymsQuery(GetAllToponymsRequestDTO request)
-    : IRequest<Result<GetAllToponymsResponseDTO>>;
+namespace Streetcode.BLL.MediatR.Toponyms.GetAll
+{
+    public record GetAllToponymsQuery(GetAllToponymsRequestDTO request) : IRequest<Result<GetAllToponymsResponseDTO>>;
+}

b0d2e8ca405a50f49f47005699a3ba1f62dbfeb9
Kornienko11004 <135712198+Kornienko11004@users.noreply.github.com>
2024-12-20 16:36:36 +0200

    Update VideoDTO.cs to resolve conflict

--- Streetcode/Streetcode.BLL/DTO/Media/VideoDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Media/VideoDTO.cs
@@ -1,10 +1,11 @@
-namespace Streetcode.BLL.DTO.Media
+using Streetcode.BLL.DTO.AdditionalContent;
+
+namespace Streetcode.BLL.DTO.Media.Video;
+
+public class VideoDTO
 {
-    public class VideoDTO
-    {
-        public int Id { get; set; }
-        public string? Description { get; set; }
-        public string? Url { get; set; }
-        public int StreetcodeId { get; set; }
-    }
+  public int Id { get; set; }
+  public string? Description { get; set; }
+  public string? Url { get; set; }
+  public int StreetcodeId { get; set; }
 }

ce1c6da503c52f218482590e71325639dbc9271e
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2024-12-21 05:07:52 +0200

    Revert " feat/SSAD-148: Added text creation feature with validation and tests"
    This reverts commit 41902b4fb42041c01a6b238a86aea3fb777a51e5.

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Text/Create/CreateTextCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using FluentResults;
-using MediatR;
-using Streetcode.BLL.DTO.Streetcode.TextContent.Text;
-
-namespace Streetcode.BLL.MediatR.Streetcode.Text.Create
-{
-    public record CreateTextCommand(TextCreateDTO TextCreateDto) : IRequest<Result<TextDTO>>;
-}

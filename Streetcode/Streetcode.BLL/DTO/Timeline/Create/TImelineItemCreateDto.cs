b01e4190905d9b7858b8c2411470d42fdeab382c
Gorgggulia <131769387+Gorgggulia@users.noreply.github.com>
2024-12-22 19:06:41 +0200

    Update TImelineItemCreateDto.cs

--- Streetcode/Streetcode.BLL/DTO/Timeline/Create/TImelineItemCreateDto.cs
+++ Streetcode/Streetcode.BLL/DTO/Timeline/Create/TImelineItemCreateDto.cs
@@ -10,6 +10,5 @@ namespace Streetcode.BLL.DTO.Timeline.Create
         public DateViewPattern DateViewPattern { get; set; }
         public IEnumerable<HistoricalContextDTO>? HistoricalContexts { get; set; } = new List<HistoricalContextDTO>();
         public int StreetcodeId { get; set; }
-        public string Context { get; set; }
     }
 }

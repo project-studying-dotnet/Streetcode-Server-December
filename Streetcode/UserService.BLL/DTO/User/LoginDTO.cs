57c6e0ca187f60c48163c1641d73171071707756
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2024-12-23 21:58:47 +0200

    feat/142: update loginDTO

--- Streetcode/UserService.BLL/DTO/User/LoginDTO.cs
+++ Streetcode/UserService.BLL/DTO/User/LoginDTO.cs
@@ -9,8 +9,6 @@ namespace UserService.BLL.DTO.User
     public class LoginDTO
     {
         public string UserName { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
     }
 }

06234c33a3f8c8a93a9ceae4848697e56d02340f
Gorgggulia <131769387+Gorgggulia@users.noreply.github.com>
2024-12-24 21:50:52 +0200

    Update CreateFactDTO.cs

--- Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Fact/CreateFactDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Fact/CreateFactDTO.cs
@@ -6,8 +6,6 @@ namespace Streetcode.BLL.DTO.Streetcode.TextContent.Fact
     {
         public string Title { get; set; }
         public string FactContent { get; set; }
-        public string Text { get; set; }
-        public string ImageDescription { get; set; }
         public int StreetcodeId { get; set; }
         public CreateFactImageDTO Image { get; set; }
     }

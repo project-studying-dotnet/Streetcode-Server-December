dcd4760239638c4a71312ad4b1c93722178612c9
Olia <helga.t@i.ua>
2024-12-29 00:19:46 +0200

    feat/172: Create FluentValidation for StreetcodeMainPageCreateDTO and StreetcodeTagDTO

---
+++ Streetcode/Streetcode.BLL/Validation/Validators/DTOValidators/AdditionalContent/Tag/StreetcodeTagDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Streetcode.BLL.DTO.AdditionalContent.Tag;
+using Streetcode.BLL.DTO.Streetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.Validation.Validators.DTOValidators.AdditionalContent.Tag
+{
+    public class StreetcodeTagDTOValidator : AbstractValidator<StreetcodeTagDTO>
+    {
+        public StreetcodeTagDTOValidator()
+        {
+            RuleFor(x => x.Title)
+                .MaximumLength(50).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+        }
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Validation/Validators/DTOValidators/Streetcode/StreetcodeMainPageCreateDTOValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Streetcode.BLL.DTO.Streetcode;
+using Streetcode.BLL.Validation.Validators.DTOValidators.AdditionalContent.Tag;
+using Streetcode.BLL.Validation.Validators.DTOValidators.Art.Audio;
+using Streetcode.BLL.Validation.Validators.DTOValidators.Art.Image;
+
+namespace Streetcode.BLL.Validation.Validators.DTOValidators.Streetcode
+{
+    public class StreetcodeMainPageCreateDTOValidator : AbstractValidator<StreetcodeMainPageCreateDTO>
+    {
+        public StreetcodeMainPageCreateDTOValidator()
+        {
+            RuleFor(x => x.Index)
+                .NotEmpty().WithMessage("The '{PropertyName}' field is required");
+            RuleFor(x => x.Title)
+                .MaximumLength(100).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+            RuleFor(x => x.FirstName)
+                .MaximumLength(50).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+            RuleFor(x => x.LastName)
+                .MaximumLength(50).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+            RuleFor(x => x.Teaser)
+                .MaximumLength(450).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+            RuleFor(x => x.EventStartOrPersonBirthDate)
+                .NotEmpty().WithMessage("The '{PropertyName}' field is required");
+            RuleFor(x => x.TransliterationUrl)
+                .NotEmpty().WithMessage("The '{PropertyName}' field is required")
+                .MaximumLength(100).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+            RuleFor(x => x.BriefDescription)
+                .MaximumLength(33).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+            RuleForEach(x => x.Tags)
+                .SetValidator(new StreetcodeTagDTOValidator());
+            RuleFor(x => x.Images)
+                .Must(m => m.Count(c => c.MimeType == "gif") <= 1);
+            RuleFor(x => x.Audio)
+                .SetValidator(new AudioFileBaseCreateDTOValidator());
+        }
+    }
+}

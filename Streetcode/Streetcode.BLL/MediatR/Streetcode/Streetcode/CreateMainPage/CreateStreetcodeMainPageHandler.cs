75f90149fd9560c6e6b4dd5141ad02c4fe71ced5
Olia <helga.t@i.ua>
2024-12-29 00:15:26 +0200

    feat/172: create CreateStreetcodeMainPage handler and command

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/CreateMainPage/CreateStreetcodeMainPageHandler.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode;
+using Streetcode.BLL.Interfaces.Audio;
+using Streetcode.BLL.Interfaces.Image;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Resources;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Entities.Streetcode.Types;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.CreateMainPage
+{
+    public class CreateStreetcodeMainPageHandler : IRequestHandler<CreateStreetcodeMainPageCommand, Result<StreetcodeDTO>>
+    {
+        private readonly IRepositoryWrapper _repository;
+        private readonly IMapper _mapper;
+        private readonly ILoggerService _logger;
+        private readonly IImageService _imageService;
+        private readonly IAudioService _audioService;
+
+        public CreateStreetcodeMainPageHandler(IRepositoryWrapper repository, IMapper mapper, ILoggerService logger, IImageService imageService, IAudioService audioService)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _logger = logger;
+            _imageService = imageService;
+            _audioService = audioService;
+        }
+
+        public async Task<Result<StreetcodeDTO>> Handle(CreateStreetcodeMainPageCommand request, CancellationToken cancellationToken)
+        {
+            if (request.StreetcodeMainPage is null)
+            {
+                string errorMsg = ErrorManager.GetCustomErrorText("FailCreateError", "main page block");
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+
+            StreetcodeContent mainPage;
+
+            if (request.StreetcodeMainPage.StreetcodeType == DAL.Enums.StreetcodeType.Person)
+            {
+                mainPage = _mapper.Map<PersonStreetcode>(request.StreetcodeMainPage);
+            }
+            else
+            {
+                mainPage = _mapper.Map<EventStreetcode>(request.StreetcodeMainPage);
+            }
+
+            // Create Animation and Picture images
+
+            if (mainPage.Images is not null && mainPage.Images.Any())
+            {
+                for (int i = 0; i < mainPage.Images.Count; i++)
+                {
+                    mainPage.Images[i] = _imageService.ConfigureImage(request.StreetcodeMainPage.Images[i]);
+                }
+            }
+
+            // Create Audio
+
+            if (mainPage.Audio is not null)
+            {
+                mainPage.Audio = _audioService.ConfigureAudio(request.StreetcodeMainPage.Audio);
+            }
+
+            var createdMainPage = await _repository.StreetcodeRepository.CreateAsync(mainPage);
+
+            if (createdMainPage is null)
+            {
+                string errorMsg = ErrorManager.GetCustomErrorText("FailCreateError", "main page block");
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+
+            var resultIsSuccess = await _repository.SaveChangesAsync() > 0;
+
+            if (!resultIsSuccess)
+            {
+                string errorMsg = ErrorManager.GetCustomErrorText("FailCreateError", "main page block");
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+
+            return Result.Ok(_mapper.Map<StreetcodeDTO>(createdMainPage));
+        }
+    }
+}

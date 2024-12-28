b5121a2088d5b4ece85666db9d56123d739f35d8
Olia <helga.t@i.ua>
2024-12-29 00:13:57 +0200

    feat/172: add audio service to inject into CreateStreetcodeMainPage handler

---
+++ Streetcode/Streetcode.BLL/Interfaces/Audio/IAudioService.cs
@@ -0,0 +1,9 @@
+﻿using Streetcode.BLL.DTO.Media.Audio;
+
+namespace Streetcode.BLL.Interfaces.Audio
+{
+    public interface IAudioService
+    {
+        public DAL.Entities.Media.Audio ConfigureAudio(AudioFileBaseCreateDTO audioDTO);
+    }
+}

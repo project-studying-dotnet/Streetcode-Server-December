a884f7f3d69beedf3946734ba99d1ebd0cdc38f3
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2024-12-29 22:36:54 +0200

    feat/SSAD-165/added-updatecategorycontent-handler

---
+++ Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/UpdateCategoryContent/UpdateCategoryContentCommand.cs
@@ -0,0 +1,13 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Sources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.MediatR.Sources.SourceLinkCategory.UpdateCategoryContent
+{
+	public record UpdateCategoryContentCommand(CategoryContentCreateDTO CategoryContent) : IRequest<Result<CategoryContentCreateDTO>>;
+}

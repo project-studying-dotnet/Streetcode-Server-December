59e0665a18c51524aa845675f0abf2a1a3f444fb
Olia <helga.t@i.ua>
2024-12-29 13:53:22 +0200

    feat/172: add CreateStreetcodeMainPageCommandValidator, improve StreetcodeMainPage dto validator

---
+++ Streetcode/Streetcode.BLL/Validation/Validators/CommandsAndQuerysValidators/CreateStreetcodeMainPageCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.CreateMainPage;
+using Streetcode.BLL.Validation.Validators.DTOValidators.Streetcode;
+
+namespace Streetcode.BLL.Validation.Validators.CommandsAndQuerysValidators
+{
+    public class CreateStreetcodeMainPageCommandValidator : AbstractValidator<CreateStreetcodeMainPageCommand>
+    {
+        public CreateStreetcodeMainPageCommandValidator()
+        {
+            RuleFor(x => x.StreetcodeMainPage)
+                .NotNull().WithMessage("StreetcodeMainPage object is required")
+                .SetValidator(new StreetcodeMainPageCreateDTOValidator());
+        }
+    }
+}
--- Streetcode/Streetcode.BLL/Validation/Validators/DTOValidators/Streetcode/StreetcodeMainPageCreateDTOValidator.cs
+++ Streetcode/Streetcode.BLL/Validation/Validators/DTOValidators/Streetcode/StreetcodeMainPageCreateDTOValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Streetcode.BLL.DTO.Streetcode;
 using Streetcode.BLL.Validation.Validators.DTOValidators.AdditionalContent.Tag;
+using Streetcode.BLL.Validation.Validators.DTOValidators.Art;
 using Streetcode.BLL.Validation.Validators.DTOValidators.Art.Audio;
 using Streetcode.BLL.Validation.Validators.DTOValidators.Art.Image;
 
@@ -11,7 +12,8 @@ namespace Streetcode.BLL.Validation.Validators.DTOValidators.Streetcode
         public StreetcodeMainPageCreateDTOValidator()
         {
             RuleFor(x => x.Index)
-                .NotEmpty().WithMessage("The '{PropertyName}' field is required");
+                .NotEmpty().WithMessage("The '{PropertyName}' field is required")
+                .GreaterThanOrEqualTo(0).WithMessage("{PropertyName} must be greater than or equal to 0");
             RuleFor(x => x.Title)
                 .MaximumLength(100).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
             RuleFor(x => x.FirstName)
@@ -24,15 +26,20 @@ namespace Streetcode.BLL.Validation.Validators.DTOValidators.Streetcode
                 .NotEmpty().WithMessage("The '{PropertyName}' field is required");
             RuleFor(x => x.TransliterationUrl)
                 .NotEmpty().WithMessage("The '{PropertyName}' field is required")
-                .MaximumLength(100).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
+                .MaximumLength(100).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters")
+                .Matches("^[a-z0-9-]+$")
+                .WithMessage("{PropertyName} is allowed to accept lowercase latin letters, numbers, and hyphens.");
             RuleFor(x => x.BriefDescription)
                 .MaximumLength(33).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters");
             RuleForEach(x => x.Tags)
                 .SetValidator(new StreetcodeTagDTOValidator());
             RuleFor(x => x.Images)
-                .Must(m => m.Count(c => c.MimeType == "gif") <= 1);
+                .Must(m => m.Count(c => c.MimeType == "gif") <= 1).WithMessage("There should be only one 'gif' image")
+                .Must(m => m.Count <= 2).WithMessage("There should be no more than one of each image")
+                .ForEach(f => f.SetValidator(new ImageFileBaseCreateDTOValidator()));
             RuleFor(x => x.Audio)
-                .SetValidator(new AudioFileBaseCreateDTOValidator());
+                .SetValidator(new AudioFileBaseCreateDTOValidator())
+                .When(w => w.Audio != null);
         }
     }
 }

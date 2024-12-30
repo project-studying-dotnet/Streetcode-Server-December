2de6f3526fc8cf5ac21e84a75a9212b93d476e15
Olia <helga.t@i.ua>
2024-12-30 20:29:01 +0200

    feat/240: create GetCommentsToReview dto

---
+++ Streetcode/Streetcode.BLL/DTO/Comment/GetCommentsToReviewDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Streetcode.BLL.DTO.Comment
+{
+    public class GetCommentsToReviewDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserFullName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DateModified { get; set; }
+        public string Content { get; set; }
+        public int StreetcodeId { get; set; }
+    }
+}

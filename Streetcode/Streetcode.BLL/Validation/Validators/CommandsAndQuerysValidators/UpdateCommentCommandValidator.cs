63ea1585c7ab4798d68b5b5961a1d1a18f5701e7
KolyaParnak <hgy.hgyich@gmail.com>
2024-12-31 01:23:03 +0200

    feat/223/3/added-fluent-validation

---
+++ Streetcode/Streetcode.BLL/Validation/Validators/CommandsAndQuerysValidators/UpdateCommentCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Comment.UpdateComment;
+using Streetcode.BLL.Validation.Validators.DTOValidators.Command;
+
+namespace Streetcode.BLL.Validation.Validators.CommandsAndQuerysValidators
+{
+    public class UpdateCommentCommandValidator : AbstractValidator<UpdateCommentCommand>
+    {
+        public UpdateCommentCommandValidator()
+        {
+            RuleFor(x => x.updateCommentDto)
+                .NotNull().WithMessage("UpdateCommentDto object is required")
+                .SetValidator(new UpdateCommentDtoValidator());
+        }
+    }
+}

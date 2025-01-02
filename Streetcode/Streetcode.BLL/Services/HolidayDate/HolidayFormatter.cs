c4c1d849bac8ac5c3b031f6f47af7b4b83bd71ea
KolyaParnak <hgy.hgyich@gmail.com>
2025-01-02 15:13:51 +0200

    feat/302/3/create-services-to-format-and-handle-response

---
+++ Streetcode/Streetcode.BLL/Services/HolidayDate/HolidayFormatter.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Streetcode.BLL.Interfaces.HolidayFormatter;
+using Streetcode.DAL.Entities.AdditionalContent.HolidayDate;
+using System.Text.Json;
+
+namespace Streetcode.BLL.Services.HolidayFormatter
+{
+    public class HolidayFormatter : IHolidayFormatter
+    {
+        public string GeneratePrompt(int day, int month, List<string> holidays)
+        {
+            return $"Создай JSON где ключ 'date' содержит дату {day}.{month}, " +
+                   $"а ключ 'holidays' содержит список этих праздников (ключ 'holidayName'): {string.Join(", ", holidays)}" +
+                   "и описание праздника придумай сам с ключом 'holidayDescription', в описание добавь про историю праздника, описание минимум 7 предложений." +
+                   "В ответе возвращай только JSON но не в виде кода а просто одной строкой без переходов.Описание на украинском языке";
+        }
+
+        public HolidayResponse ParseResponse(string jsonResponse)
+        {
+            var formattedResponse = JsonDocument.Parse(jsonResponse)
+                .RootElement
+                .GetProperty("choices")[0]
+                .GetProperty("message")
+                .GetProperty("content")
+                .GetString()
+                .Trim('"');
+
+            return JsonConvert.DeserializeObject<HolidayResponse>(formattedResponse);
+        }
+    }
+}

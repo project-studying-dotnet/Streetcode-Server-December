b067100dd3b6bad74447395a7a8523a065e26009
Harapko <harapkomaskym@gmail.com>
2025-01-05 19:47:40 +0200

    enhancement/321/add azure service bus to emailMicroservice

---
+++ Streetcode/EmailService.BLL/Services/Azure/AzureServiceBus.cs
@@ -0,0 +1,52 @@
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using EmailService.BLL.Interfaces.Azure;
+
+namespace EmailService.BLL.Services.Azure;
+
+public class AzureServiceBus(string conn) : IAzureServiceBus
+{
+    public async Task SendMessage(string queueName, string message)
+    {
+        try
+        {
+            var sbClientOption = new ServiceBusClientOptions
+            {
+                TransportType = ServiceBusTransportType.AmqpWebSockets,
+            };
+
+            var sbClient = new ServiceBusClient(conn, sbClientOption);
+            var sbSender = sbClient.CreateSender(queueName);
+
+            await sbSender.SendMessageAsync(new ServiceBusMessage(message));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public async Task<string> ReceiveMessage(string queueName)
+    {
+        try
+        {
+            var sbClientOption = new ServiceBusClientOptions
+            {
+                TransportType = ServiceBusTransportType.AmqpWebSockets,
+            };
+            
+            var sbClient = new ServiceBusClient(conn, sbClientOption);
+            var sbReceiver = sbClient.CreateReceiver(queueName);
+            var msg = await sbReceiver.ReceiveMessageAsync();
+            var msgBody = Encoding.UTF8.GetString(msg.Body);
+
+            return msgBody;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
--- Streetcode/EmailService.WebApi/Program.cs
+++ Streetcode/EmailService.WebApi/Program.cs
@@ -1,68 +1,66 @@
 using EmailService.BLL.Interfaces;
+using EmailService.BLL.Interfaces.Azure;
 using EmailService.BLL.Services;
+using EmailService.BLL.Services.Azure;
 using EmailService.DAL.Entities;
 using EmailService.BLL.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Serilog;
 
-namespace EmailService.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration) 
+    .Enrich.FromLogContext() 
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+
+// Add services to the container.
+
+builder.Services.AddFluentValidationAutoValidation(); 
+builder.Services.AddValidatorsFromAssemblyContaining<EmailDtoValidator>();
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton(Log.Logger);
+
+var azureServiceBusConn = builder.Configuration.GetConnectionString("ServiceBusConn")!;
+
+
+var emailConfig = builder.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+builder.Services.AddSingleton(emailConfig);
+
+var currentAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(currentAssemblies));
+
+builder.Services.AddScoped<IEmailService, EmailService.BLL.Services.EmailService>();
+builder.Services.AddScoped<ILoggerService, LoggerService>();
+
+builder.Services.AddSingleton<IAzureServiceBus, AzureServiceBus>(sb =>
+    new AzureServiceBus(azureServiceBusConn));
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) 
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Configuration) 
-                .Enrich.FromLogContext() 
-                .WriteTo.Console()
-                .CreateLogger();
-
-            builder.Host.UseSerilog();
-
-
-            // Add services to the container.
-
-            builder.Services.AddFluentValidationAutoValidation(); 
-            builder.Services.AddValidatorsFromAssemblyContaining<EmailDtoValidator>();
-
-            builder.Services.AddControllers();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddSingleton<Serilog.ILogger>(Log.Logger);
-
-
-            var emailConfig = builder.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
-            builder.Services.AddSingleton(emailConfig);
-
-            var currentAssemblies = AppDomain.CurrentDomain.GetAssemblies();
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(currentAssemblies));
-
-            builder.Services.AddScoped<IEmailService, BLL.Services.EmailService>();
-            builder.Services.AddScoped<ILoggerService, LoggerService>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment()) 
-            {
-                app.UseSwagger(); 
-                app.UseSwaggerUI();
-            }
-            
-
-          
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger(); 
+    app.UseSwaggerUI();
 }
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
+

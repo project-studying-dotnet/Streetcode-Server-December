ebdb8a3b471a1bfc0063f444ec4d21952b071b32
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2025-01-12 23:49:12 +0200

    Setup test environment

---
+++ Streetcode/Streetcode.XIntegrationTest/Factories/StreetcodeWebApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Streetcode.DAL.Persistence;
+using Streetcode.XIntegrationTest.Factories.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.XIntegrationTest.Factories
+{
+    internal class StreetcodeWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<StreetcodeDbContext>));
+
+                services.AddDbContext<StreetcodeDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryStreetcodeDb");
+                });
+
+                services.AddAuthentication("TestScheme")
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                    "Test", options => { });
+            });
+        }
+    }
+}

1f04a3c276e9496fadcbce11b35cd79526666f21
Maksym <150162309+Harapko@users.noreply.github.com>
2025-01-13 16:12:55 +0200

    update Idle Timeout

--- Streetcode/Streetcode.WebApi/Program.cs
+++ Streetcode/Streetcode.WebApi/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.IdleTimeout = TimeSpan.FromDays(7);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
